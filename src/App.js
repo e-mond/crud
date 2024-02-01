5e2fdb7f76d9c3b7661fd96cdb4c5dd1c4605779
Mond <126099833+e-mond@users.noreply.github.com>
2024-02-01 18:25:46 +0000

    Creating And Updating

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import './App.css';
 
 function App() {
+
+  // formData state, witha s stater function called SetformData
+  //use state with a function to change whatever is in the form
+  // This stores the data in the form
+  const [formData, setFormData] = useState({name: "",age: ""});
+
+  const [users, setUsers] = useState([])
+  
+  const handleChange = (e) =>{
+    let { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value
+    }));
+    console.log(formData);
+  };
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+        setUsers([...users, formData ])
+    console.log(users,formData,"data ff")
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col md="12">
+
+          <h1>SIGN UP</h1>
+
+          <form onSubmit={handleSubmit}>
+            <label>Name</label>
+            <br />
+            <input
+              type="text"
+              placeholder="Username"
+              value={formData.name}
+              onChange={handleChange}
+              name="name"
+            />
+            <br />
+
+            <label>Age</label>
+            <br />
+            <input
+              type="text"
+              placeholder="Age"
+              value={formData.age}
+              onChange={handleChange}
+              name="age"
+            />
+            <br />
+
+            <br />
+            <input type="submit" />
+          </form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
