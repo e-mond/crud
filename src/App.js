ad190bab8bbabb9037dff4a73b0e3e51b1d6251e
Mond <126099833+e-mond@users.noreply.github.com>
2024-02-02 15:56:36 +0000

    Displaying Users

--- src/App.js
+++ src/App.js
@@ -23,14 +23,15 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-        setUsers([...users, formData ])
-    console.log(users,formData,"data ff")
+        setUsers([...users, formData ]) //because the user is in an error, we call it using the []. This is the Form Operator
+    console.log(users,formData,"data ff") // Console.log user state to see if it's been updated
 
   };
 
   return (
     <Container>
       <Row>
+
         <Col md="12">
 
           <h1>SIGN UP</h1>
@@ -59,11 +60,27 @@ function App() {
             <br />
 
             <br />
-            <input type="submit" />
+            <input type="submit" /> 
+            {/* The Event listerner i.e OnsUbmit helps us to submit the form */}
           </form>
         </Col>
       </Row>
+
+      
+       {/* Displaying Users */}
+       <Row>
+
+        <Col md="12">
+          {users.map((user)=>(<>
+          <h2>{user.name}</h2>
+          <p>{user.age}</p>
+          </>))}
+
+
+        </Col>
+      </Row>
     </Container>
+    
   );
 }
 
